e97da2f90bd42a591a353839e7aa884c4b1fe852
Andre Wyrwa <andre.wyrwa@theiconic.com.au>
2015-06-25 12:16:09 +1000

    finalise reveal.js & gh-pages talk

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,7 +96,7 @@ module.exports = function(grunt) {
             },
             normalized: {
                 options: {
-                    use: [mozjpeg()]
+                    use: [mozjpeg({quality: 90})]
                 },
                 files: [{
                     expand: true,
